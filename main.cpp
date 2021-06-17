5b987ddb69f316ab75e2ffc27279c64430e5d270
Freddy <frederik@bussmann.io>
2021-06-17 13:11:41 +0200

    Updated parseInput function

--- main.cpp
+++ main.cpp
@@ -17,54 +17,48 @@ Graph parseInput()
     Graph* graph;
 
     std::stringstream output;
-    std::string line;
 
     bool inputParsed = false;
     bool edgeInputMode = false;
 
     while (!inputParsed) {
-        getline(std::cin, line);
+        std::string lineInput;
+        getline(std::cin, lineInput);
+        std::stringstream line;
+        line << lineInput;
 
-        if (line.length() == 0) {
+        if (lineInput.length() == 0) {
             inputParsed = true;
         }
 
-        if (line.length() < 3) {
-            if (std::isdigit(line[0]) == 1) {
-                int size = std::stoi(line);
+        if (lineInput.length() < 3) {
+            if (std::isdigit(lineInput[0]) == 1) {
+                int size = std::stoi(lineInput);
                 graph = new Graph(size);
             }
-            else if (line == "E") {
+            else if (lineInput == "E") {
                 edgeInputMode = true;
             }
         }
-        else if (line.length() >= 3) {
-            if (!edgeInputMode && line.length() >= 7) {
-                std::string name = line.substr(2, 2);
-                std::string shape = line.substr(0, 1) == "H" ? "box" : "circle";
-                float x = std::stoi(line.substr(5, 1));
-                float y = std::stoi(line.substr(7, 1));
+        else if (lineInput.length() >= 3) {
+            if (!edgeInputMode && lineInput.length() >= 7) {
+                std::string name = lineInput.substr(2, 2);
+                std::string shape = lineInput.substr(0, 1) == "H" ? "box" : "circle";
+
+                float x = std::stoi(lineInput.substr(5, 1));
+                float y = std::stoi(lineInput.substr(7, 1));
 
                 Node* node = new Node(name, shape, x, y);
                 graph->addNode(*node);
             }
             else {
-                int nodeIndexA = std::stoi(line.substr(0, 1));
-                int nodeIndexB = -1;
+                int nodeIndexA;
+                int nodeIndexB;
 
-                if (line.length() == 3) {
-                    nodeIndexB = std::stoi(line.substr(2, 1));
-                }
-                else if (line.length() == 4) {
-                    nodeIndexB = std::stoi(line.substr(2, 2));
-                }
+                line >> nodeIndexA;
+                line >> nodeIndexB;
 
-                if (nodeIndexB != -1) {
-                    graph->connect(nodeIndexA, nodeIndexB);
-                }
-                else {
-                    std::cout << "Error while parsing node connection at: " << line << std::endl;
-                }
+                graph->connect(nodeIndexA, nodeIndexB);
             }
         }
     }
